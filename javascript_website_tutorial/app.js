6eb0091d84edcec28ba36066c30ca2dbf6d194f5
yukito-kaneshige <128114617+yukito-kaneshige@users.noreply.github.com>
2023-04-02 14:50:06 +0900

    Add files via upload
    javascriptを用いた動的なwebサイトをつくりました

---
+++ javascript_website_tutorial/app.js
@@ -0,0 +1,22 @@
+var circle = document.getElementById("circle");
+var upBtn = document.getElementById("upBtn");
+var downBtn = document.getElementById("downBtn");
+
+var rotateValue = circle.style.transform;
+// console.log(rotateValue);
+var rotateSum;
+
+upBtn.onclick = function() {
+    /*時計回りに90度回転させる。*/
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
+
+downBtn.onclick = function() {
+    /*半時計回りに90度回転させる。*/
+    rotateSum = rotateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
+
